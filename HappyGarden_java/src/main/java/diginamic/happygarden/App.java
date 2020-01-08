e82aa1c66ffb266a70534c8febc806a50ce799ac
BriceB <brice.beteille@gmail.com>
2020-01-08 10:03:30 +0100

    starting project : added .gitignore and dependency in java, java and angular directory

---
+++ HappyGarden_java/src/main/java/diginamic/happygarden/App.java
@@ -0,0 +1,13 @@
+package diginamic.happygarden;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
