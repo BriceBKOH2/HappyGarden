55f3eba220cf2ea52003e8f817b91907a3be8128
Estelle <estelle.idee@gmail.com>
2020-01-17 16:41:00 +0100

    added sidebar but need to be modified and filled in

---
+++ happygarden-angular/src/app/navigation/nav-garden/nav-garden.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-nav-garden",
+  templateUrl: "./nav-garden.component.html",
+  styleUrls: ["./nav-garden.component.scss"]
+})
+export class NavGardenComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ happygarden-angular/src/app/navigation/navigation.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { NavGardenComponent } from "./nav-garden/nav-garden.component";
+import { SidebarModule } from "ng-sidebar";
+
+@NgModule({
+  declarations: [NavbarComponent, NavGardenComponent],
+  exports: [NavbarComponent, NavGardenComponent],
+  imports: [CommonModule, SidebarModule.forRoot()]
+})
+export class NavigationModule {}
