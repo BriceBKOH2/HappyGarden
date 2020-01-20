0dd9190cef8266e20d0c350201c6a6fd48920546
BriceB <brice.beteille@gmail.com>
2020-01-20 10:44:27 +0100

    change .editcongit to resolve singlequote conflict

--- happygarden-angular/src/app/navigation/nav-garden/nav-garden.component.ts
+++ happygarden-angular/src/app/navigation/nav-garden/nav-garden.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-nav-garden",
-  templateUrl: "./nav-garden.component.html",
-  styleUrls: ["./nav-garden.component.scss"]
+  selector: 'app-nav-garden',
+  templateUrl: './nav-garden.component.html',
+  styleUrls: ['./nav-garden.component.scss']
 })
 export class NavGardenComponent implements OnInit {
   constructor() {}
