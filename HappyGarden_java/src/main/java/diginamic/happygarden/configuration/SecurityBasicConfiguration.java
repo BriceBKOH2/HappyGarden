351e285f31b113f6dcc00e28415ec702eff69740
BriceB <brice.beteille@gmail.com>
2020-01-23 11:25:35 +0100

    added csrf to security config in java

--- HappyGarden_java/src/main/java/diginamic/happygarden/configuration/SecurityBasicConfiguration.java
+++ HappyGarden_java/src/main/java/diginamic/happygarden/configuration/SecurityBasicConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +33,9 @@ public class SecurityBasicConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/Admin/**").hasRole("ADMIN")
 			.anyRequest().authenticated()
 			.and().httpBasic()
-			.and().csrf().disable();
+			.and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository
+						.withHttpOnlyFalse());
 	}
 
 	@Autowired
