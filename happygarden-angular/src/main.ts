e6626f1fae5ec20e7beb14eb9fc947a016a87bf1
Lionel MEUNIER <lmeunier@iocean.fr>
2020-02-11 12:38:17 +0100

    Add debug tools

--- happygarden-angular/src/main.ts
+++ happygarden-angular/src/main.ts
@@ -1,13 +1,17 @@
-import { enableProdMode } from '@angular/core';
+import { ApplicationRef, enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { enableDebugTools } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .bootstrapModule(AppModule).then(module => {
+  const applicationRef = module.injector.get(ApplicationRef);
+  const appComponent = applicationRef.components[0];
+  enableDebugTools(appComponent);
+}).catch(err => console.error(err));
